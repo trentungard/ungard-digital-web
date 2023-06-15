a1669c5665af8e2e61ad3969e4bfdeaf48004f65
trent.ungard <trent.ungard@teamairship.com>
2023-06-15 13:59:28 -0400

    adding smartlook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import Head from 'next/head'
 import { ComingSoon } from '../components/ComingSoon';
 import '../styles/index.css'
+import Script from 'next/script';
+
+const UniversalHead = () => (
+  <>
+    <Head>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Script 
+      type='text/javascript' 
+      dangerouslySetInnerHTML={{ __html: `window.smartlook||(function(d) {
+        var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+        var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+        c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+        })(document);
+        smartlook('init', '1bcffab52314e37eee0758bae3c347b052721f4a', { region: 'eu' });` }} 
+    />
+  </>
+)
 
 function MyApp({ Component, pageProps }) {
 
@@ -8,16 +26,12 @@ function MyApp({ Component, pageProps }) {
 
   return isMaintenanceMode ? (
     <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <UniversalHead />
       <ComingSoon />
     </>
   ) : (
     <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <UniversalHead />
       <Component {...pageProps} />
     </>
   )
