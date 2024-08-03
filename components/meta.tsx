0ab23b0c280c7a4a35332c1fe921fc5fc63e889e
Trent Ungard <trentungard@Trents-MacBook-Pro.local>
2024-08-03 01:23:15 -0400

    desc

--- components/meta.tsx
+++ components/meta.tsx
@@ -34,7 +34,7 @@ export default function Meta() {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`Affordable software engineering based out of Williamsport, serving the greater Lycoming County area`}
+        content="Affordable software engineering & web development based out of Williamsport, serving the greater Lycoming County area"
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} key="ogImage" />
     </Head>
